18c263f30e69b2d9dc6d52a6aa8d36b90a7dfbe6
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-12-07 20:41:11 -0500

    :fire::horse: fix browserify error

--- test/test.info.js
+++ test/test.info.js
@@ -17,7 +17,7 @@
 'use strict';
 
 const assert = require('chai').assert;
-const PersonalityTraitInfo  = require('../src/index');
+const PersonalityTraitInfo  = require('../lib/index');
 
 describe('info', () => {
 
